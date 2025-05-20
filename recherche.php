dd27e436cc900a03497c807e27a818c167294bd7
Amine954 <amine.95lcc@gmail.com>
2025-05-20 21:18:13 +0200

    ajout recherche voyages

--- recherche.php
+++ recherche.php
@@ -34,29 +34,50 @@ if (session_status() === PHP_SESSION_NONE) {
     </div>
 
     <div id="informations">
+        <form action="recherche.php" method="get">
+
         <div class="info-formulaires">
             <label for="pays"><i class="fas fa-globe-europe"></i> Au départ de </label>
             <select name="pays" id="pays">
-                <option value="">Choisir une ville</option>
-                <option value="st">Stockholm</option>
-                <option value="co">Copenhague</option>
-                <option value="os">Oslo</option>
-            </select>      
+                <option value="">Choisir une ville</option> 
+
+                <?php
+                    $fichier_voy = fopen("donnees/voyages.csv", "r") or die("Impossible d'ouvrir le fichier !");
+
+                        $liste_villes = array();
+
+                        while(!feof($fichier_voy)){
+
+                            $voy = fgets($fichier_voy);
+                            //Si la ligne n'est pas vide (une ligne vide a un caractère " " et "\n" d'où >2)
+                            if(strlen($voy) > 2){   
+
+                                $infos_voy = str_getcsv($voy, ";", " ");
+                                
+                                $nom = $infos_voy[0];
+                                
+                                $prix = $infos_voy[1];
+                                $presentation = $infos_voy[4];
+                                $lien = $infos_voy[5];
+                                $ville = $infos_voy[6];
+
+                                if(!in_array($ville, $liste_villes)){
+                                    $liste_villes[] = $ville;
+                                    echo '<option value="'. $ville .'">'. $ville .'</option>';
+                                }                               
+                            }
+                        }   
+
+                fclose($fichier_voy);
+            ?> 
+            </select>  
+                      
         </div>
-        
-        <div class="info-formulaires">
-            <label for="date_de_depart"><i class="far fa-calendar-alt"></i> Date de départ</label>
-            <input type="date" name="date_de_depart" id="date_de_depart" />
-        </div>
-        
-        <div class="info-formulaires">
-            <label for="recherche"><i class="fas fa-search"></i> Mot-clé</label>
-            <input type="text" id="recherche" name="recherche" placeholder="Rechercher une destination..." />
-        </div>
-        
+
         <div class="info-formulaires">
             <button class="cta-button">Rechercher</button>
         </div>
+        </form>  
     </div>
 
     <section class="best-voyages">
@@ -69,33 +90,35 @@ if (session_status() === PHP_SESSION_NONE) {
 
                     $voy = fgets($fichier_voy);
                     //Si la ligne n'est pas vide (une ligne vide a un caractère " " et "\n" d'où >2)
+
                     if(strlen($voy) > 2){
-
                                 
-                $infos_voy = str_getcsv($voy, ";", " ");
-                
-                $nom = $infos_voy[0];
-                $prix = $infos_voy[1];
-                $presentation = $infos_voy[4];
-                $lien = $infos_voy[5];
+                        $infos_voy = str_getcsv($voy, ";", " ");
+                        
+                        $nom = $infos_voy[0];
+                        $prix = $infos_voy[1];
+                        $presentation = $infos_voy[4];
+                        $lien = $infos_voy[5];
 
-                echo '<div class="voyage-item">';
-                echo   '<img src="'. $lien .'"alt="'. $nom .'">';
-                echo   '<div class="voyage-content">';
-                echo        '<h3>'. $nom .'</h3>';
-                echo        '<p>' . $presentation .' </p>';
-                echo        '<p class="voyage-price">À partir de : '. $prix .' € / nuit</p>';
-                echo            '<div class="voyage-buttons">';
-                echo                '<a href="voyage.php?nom='.$nom.'.php" class="boutonmenu">En savoir plus</a>';
-                echo                '<a href="reservation.php?destination='.$nom.'" class="boutonmenu">Réserver</a>';
-                echo            '</div>';
-                echo    '</div>';
-                echo '</div>';    
-            }
-        }
+                        if((isset($_GET['pays']) && $_GET['pays'] == $infos_voy[6]) || !isset($_GET['pays']) || $_GET['pays'] == ""){
+                            echo '<div class="voyage-item">';
+                            echo   '<img src="'. $lien .'"alt="'. $nom .'">';
+                            echo   '<div class="voyage-content">';
+                            echo        '<h3>'. $nom .'</h3>';
+                            echo        '<p>' . $presentation .' </p>';
+                            echo        '<p class="voyage-price">À partir de : '. $prix .' € / nuit</p>';
+                            echo            '<div class="voyage-buttons">';
+                            echo                '<a href="voyage.php?nom='.$nom.'" class="boutonmenu">En savoir plus</a>';
+                            echo                '<a href="reservation.php?destination='.$nom.'" class="boutonmenu">Réserver</a>';
+                            echo            '</div>';
+                            echo    '</div>';
+                            echo '</div>';    
+                        }
+                    }
+                }
 
-        fclose($fichier_voy);
-        ?> 
+            fclose($fichier_voy);
+            ?> 
             
         </div>
     </section>
